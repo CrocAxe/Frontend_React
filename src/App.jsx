e30ffad35de09e9b40c7f5f6e7d405d47bb98e8a
Xoli <xolinxiweni@gmail.com>
2025-01-14 11:09:23 +0200

    project setup using react and vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
+git init
+git add .
+git commit -m "project setup using react and vite"
+git branch -M main
+git remote add origin https://github.com/CrocAxe/Frontend_React.git
+git push -u origin main
